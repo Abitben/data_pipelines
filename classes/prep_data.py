19a9c24ea47e4a6b369d34e8f5a7b901181cf802
Abitben <122393365+Abitben@users.noreply.github.com>
2024-02-15 00:52:23 +0100

    add a heritage for dataCNIL for prep data

--- classes/prep_data.py
+++ classes/prep_data.py
@@ -10,8 +10,6 @@ import re
 from unidecode import unidecode
 from IPython.display import display
 
-
-
 class ZipFileProcessor:
     def __init__(self, gcs_bucket_name, credentials_path, zip_blob_name, output_folder_name):
         self.gcs_bucket_name = gcs_bucket_name
@@ -53,10 +51,10 @@ class PrepFilesBQ:
         if errors_imp > 1:
             print('More than 1 percent of rows skipped, file is not good')
             df = None
-            return df
         else:
             print('More less 1 percent of rows skipped, file is okay')
-            return df
+        
+        return df
 
     def correct_shape(self, path, df):
         try:
@@ -66,16 +64,12 @@ class PrepFilesBQ:
                 if df.shape[1] == 1:
                     print('try to find headers in 2nd row')
                     df = pd.read_csv(path, sep=';', skiprows=1)
-                    return df
                 else:
                     return df
             elif 'unnamed' in str(df.columns[1]).lower():
                 print('try to find headers in 2nd row')
                 df.columns = df.iloc[0]
                 df = df[1:]
-                return df
-            else:
-                return df
         except ParserError as e:
             print(f"{Fore.RED}Exception type: {type(e).__name__}{Style.RESET_ALL}")
             print(f"{Fore.RED}Exception: {e}{Style.RESET_ALL}, return None")
@@ -87,6 +81,8 @@ class PrepFilesBQ:
             print(f"{Fore.RED}Exception: {e}{Style.RESET_ALL}, return None")
             df = None
             return df
+    
+        return df
 
     def transposed(self, df):
         if df.shape[1] > df.shape[0]:
@@ -97,10 +93,10 @@ class PrepFilesBQ:
             column1 = df.columns.name
             df = df.reset_index()
             df = df.rename(columns={df.columns[0]: column1})
-            return df
         else:
             print('More rows than columns, no need to transpose')
-            return df
+
+        return df
 
     def open_csv_file(self, path):
         try:
@@ -134,32 +130,16 @@ class PrepFilesBQ:
         new_columns = []
         for index, column in enumerate(df.columns):
             try:
-                # Remplacement des caractères spéciaux
                 column = str(column)
-                
                 column = re.sub(r'[!\"$()\*\.,\/;?\@\[\]\\^`{}\~]', '', column)
-
-                # Remplacement des espaces par des underscores et conversion en minuscules
                 column = column.strip().lower().replace(" ", "_")
-
-                # Suppression des caractères spéciaux accentués
                 column = unidecode(column)
-
-                # Remplacement de certains caractères spéciaux par des underscores
                 column = column.replace("\n", "_")
                 column = column.replace("'", "")
                 column = column.replace("-", "")
                 column = column.replace("&", 'and')
-                
-                # Suppression de certaines chaînes spécifiques dans les noms de colonnes
-                column = column.replace('https:edpbeuropaeuaboutedpbboardmembers_fr', "").replace('https:wwwafapdporglafapdpmembres', "")
-                
-                # Suppression des chevrons '<' et '>'
                 column = column.replace('<', "").replace('>', "")
-
-                # Limiter la longueur du nom de la colonne à 200 caractères
                 column = column[:200]
-
                 new_columns.append(column)
             except AttributeError:
                 new_columns.append(column)
@@ -173,13 +153,10 @@ class PrepFilesBQ:
             try: 
                 pattern = r"^[a-zA-Z0-9_]+$"
                 is_cleaned = re.match(pattern, column)
-
                 if is_cleaned and len(column) <= 200:
-                    
                     print("The column_formatter method worked perfectly.")
                 else:
                     print("The column_formatter method didn't work perfectly. Executing again the method...")
-                    # Re-run the column_clean function
                     self.columns_formatter(df)
                     break 
             except TypeError:
@@ -262,3 +239,51 @@ class PrepFilesBQ:
                 print(Fore.RED + f"{path} not processed!" + Style.RESET_ALL)
                 print("---------------------------------------------------")
     
+
+class PrepDataCnilBQ(PrepFilesBQ):
+
+    def __init__(self, paths):
+        super().__init__(paths)
+
+    def transposed(self, df):
+        if df.shape[1] > df.shape[0]:
+            print('More columns than rows, need to transpose')
+            df = df.transpose()
+            df.columns = df.iloc[0]
+            df = df[1:]  
+            column1 = df.columns.name
+            df = df.reset_index()
+            df = df.rename(columns={df.columns[0]: column1})
+        elif df.shape[1] <= df.shape[0] and df.columns[0].lower() == 'année' and str(df.columns[1]).isdigit() and len(str(df.columns[1])) == 4:
+            print('Values for years in first column, need to transpose')
+            df = df.transpose()
+            df.columns = df.iloc[0]
+            df = df[1:] 
+            column1 = df.columns.name
+            df = df.reset_index()
+            df = df.rename(columns={df.columns[0]: column1})
+        else:
+            print('More rows than columns, no need to transpose')
+    
+    def columns_formatter(self, df):
+        new_columns = []
+        for index, column in enumerate(df.columns):
+            try:
+                column = str(column)
+                column = re.sub(r'[!\"$()\*\.,\/;?\@\[\]\\^`{}\~]', '', column)
+                column = column.strip().lower().replace(" ", "_")
+                column = unidecode(column)
+                column = column.replace("\n", "_")
+                column = column.replace("'", "")
+                column = column.replace("-", "")
+                column = column.replace("&", 'and')
+                column = column.replace('https:edpbeuropaeuaboutedpbboardmembers_fr', "").replace('https:wwwafapdporglafapdpmembres', "")
+                column = column.replace('<', "").replace('>', "")
+                column = column[:200]
+                new_columns.append(column)
+            except AttributeError:
+                new_columns.append(column)
+                continue
+    
+        df.columns = new_columns
+        return df
