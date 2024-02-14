5f4b401e35077cabc090a25e2844a5a82b0a93d8
Abitben <122393365+Abitben@users.noreply.github.com>
2024-02-15 00:28:24 +0100

    preparing data for bq : remake the logic tree to open a file, and consolidate logic. TO DO : docstring, specific class for CNIL

--- classes/prep_data.py
+++ classes/prep_data.py
@@ -1,4 +1,5 @@
 import pandas as pd
+from pandas.errors import ParserError
 from google.oauth2 import service_account
 from google.cloud import storage
 import zipfile
@@ -7,6 +8,8 @@ import io
 from colorama import Fore, Style
 import re
 from unidecode import unidecode
+from IPython.display import display
+
 
 
 class ZipFileProcessor:
@@ -33,124 +36,107 @@ class ZipFileProcessor:
 
         print("Zip file processed successfully!")
 
-    def load_and_process_file(self, zip_file, file_name, bucket):
-        try:
-            print('extension found')
-            if file_name.endswith('.csv'):
-                df = self.process_csv_file(zip_file, file_name, bucket)
-                return df
-            elif file_name.endswith('.xlsx'):
-                df = self.process_excel_file(zip_file, file_name, bucket)
-                return df
-            else:
-                print('no extension found')
-                try:
-                  df = self.process_csv_file(zip_file, file_name, bucket)
-                  return df
-                except:
-                  df = self.process_excel_file(zip_file, file_name, bucket)
-                  return df
-        except Exception as e:
-            print(Fore.RED + f"Error when processing file {file_name} : {e}" + Style.RESET_ALL)
-
-    def process_csv_file(self, zip_file, file_name, bucket):
-        with zip_file.open(file_name) as file:
-            try: 
-              df = pd.read_csv(file)
-              print('read_csv with ,', 'shape :', df.shape, 'columns', df.columns)
-            except:
-              df = pd.read_csv(file, sep=";", on_bad_lines='warn')
-              print('read_csv with ;', 'shape :', df.shape, 'columns', df.columns)
-            return df
-
-    def process_excel_file(self, zip_file, file_name, bucket):
-        with zip_file.open(file_name) as file:
-            df = pd.read_excel(file)
-            print('read_excel', 'shape :', df.shape, 'columns', df.columns)
-            return df
-    
-    def reorganize_columns(self, df):
-      if self.df.shape[1] > self.df.shape[0]:
-            print('Need to transpose')
-            df = self.df.transpose()
-            df.columns = self.df.iloc[0]
-            df = self.df[1:]
-            df = self.df.reset_index(names=[self.df.columns.name])
-            df.columns.name = ""
-            return self.df
-
-    def upload_to_gcs(self, bucket, file_name, df):
-        output_blob_name = f"{self.output_folder_name}/{file_name}"
-        output_blob = bucket.blob(output_blob_name)
-        output_blob.upload_from_string(df.to_csv(index=False))
-        print(f"{file_name} processed and uploaded to GCS.")
-
-
 class PrepFilesBQ:
 
     def __init__(self, paths):
         self.paths = paths
 
-    def process_files(self):
-        for path in self.paths:
-            print(path)
-            try:
-                print('extension found')
-                if path.endswith('.csv'):
-                    df = self.open_csv_file(path)
-                    self.process_df(df, path)
-                elif path.endswith('.xlsx'):
-                    df = self.open_excel_file(path)
-                    self.process_df(df, path)
+    def verify_error_onbadlines(self, path, df):
+        with open(path) as f:
+            len_csv = sum(1 for line in f)
+
+        print(len_csv)
+        print(df.shape)
+        number_of_skipped_rows = len_csv - df.shape[0]
+        print('number_of_skipped_rows:' , number_of_skipped_rows)
+        errors_imp = number_of_skipped_rows/len_csv * 100
+        if errors_imp > 1:
+            print('More than 1 percent of rows skipped, file is not good')
+            df = None
+            return df
+        else:
+            print('More less 1 percent of rows skipped, file is okay')
+            return df
+
+    def correct_shape(self, path, df):
+        try:
+            if df.shape[1] == 1:
+                print('columns shape is 1, csv read with ;')
+                df = pd.read_csv(path, sep=';')
+                if df.shape[1] == 1:
+                    print('try to find headers in 2nd row')
+                    df = pd.read_csv(path, sep=';', skiprows=1)
+                    return df
                 else:
-                    print('no extension found')
-                    try:
-                        df = self.open_csv_file(path)
-                        self.process_df(df, path)
-                    except:
-                        df = self.open_excel_file(path)
-                        self.process_df(df, path)
-            except Exception as e:
-                print(Fore.RED + f"Error when processing file {path} : {e}" + Style.RESET_ALL)
-    
+                    return df
+            elif 'unnamed' in str(df.columns[1]).lower():
+                print('try to find headers in 2nd row')
+                df.columns = df.iloc[0]
+                df = df[1:]
+                return df
+            else:
+                return df
+        except ParserError as e:
+            print(f"{Fore.RED}Exception type: {type(e).__name__}{Style.RESET_ALL}")
+            print(f"{Fore.RED}Exception: {e}{Style.RESET_ALL}, return None")
+            print('cant correct shape')
+            df = None
+            return df
+        except Exception as e:
+            print(f"{Fore.RED}Exception type: {type(e).__name__}{Style.RESET_ALL}")
+            print(f"{Fore.RED}Exception: {e}{Style.RESET_ALL}, return None")
+            df = None
+            return df
+
+    def transposed(self, df):
+        if df.shape[1] > df.shape[0]:
+            print('More columns than rows, need to transpose')
+            df = df.transpose()
+            df.columns = df.iloc[0]
+            df = df[1:]  
+            column1 = df.columns.name
+            df = df.reset_index()
+            df = df.rename(columns={df.columns[0]: column1})
+            return df
+        else:
+            print('More rows than columns, no need to transpose')
+            return df
+
     def open_csv_file(self, path):
-        try: 
+        try:
             df = pd.read_csv(path)
-            print('read_csv with ,', 'shape :', df.shape, 'columns', df.columns)
-        except:
-            df = pd.read_csv(path, sep=";", on_bad_lines='warn')
-            print('read_csv with ;', 'shape :', df.shape, 'columns', df.columns)
-        return df
+        except ParserError as e:
+            print(f"{Fore.RED}Exception type (first attempt): {type(e).__name__}{Style.RESET_ALL}")
+            print(e)
+            print('trying to open csv with sep = ";"')
+            try:
+                df = pd.read_csv(path, sep=';')
+            except Exception as e:
+                print(f"{Fore.RED}Exception type (second attempt): {type(e).__name__}{Style.RESET_ALL}")
+                print(f"{Fore.RED}Exception: {e}{Style.RESET_ALL}")
+                print('Unable to open CSV file')
+                df = None
+
+        if df is not None:
+            print(df.shape)
+            df = self.correct_shape(path, df)
+            return df
+        else:
+            return df
 
     def open_excel_file(self, path):
         df = pd.read_excel(path)
-        print('read_excel', 'shape :', df.shape, 'columns', df.columns)
+        print(df.shape)
+        df = self.correct_shape(path, df)
         return df
     
-    def process_df(self, df, path):
-        df = self.transpose_df(df)
-        df = self.columns_formatter(df)
-        df = self.check_column_clean(df)
-        self.return_csv(df, path)
-
-    def transpose_df(self, df):
-        if df.shape[1] > df.shape[0]:
-            print('Need to transpose')
-            df = df.transpose()
-            df.columns = df.iloc[0]
-            df = df[1:]
-            # df = df.reset_index(names=[df.columns.name])
-            # df.columns.name = ""
-            return df
-        else:
-            print('No need to transpose')
-            return df
-        
     def columns_formatter(self, df):
         new_columns = []
         for index, column in enumerate(df.columns):
             try:
                 # Remplacement des caractères spéciaux
+                column = str(column)
+                
                 column = re.sub(r'[!\"$()\*\.,\/;?\@\[\]\\^`{}\~]', '', column)
 
                 # Remplacement des espaces par des underscores et conversion en minuscules
@@ -200,14 +186,79 @@ class PrepFilesBQ:
                 continue
         print("Re-exécution terminée.")
         return df
+
+
+    def open_df(self, path):
+        if path.endswith('.csv'):
+            print('.csv found')
+            df = self.open_csv_file(path)
+        elif path.endswith('.xlsx'):
+            print('.xsxl found')
+            df = self.open_excel_file(path)
+        elif "." not in path:
+            try:
+                try: 
+                    print('try to read as csv')
+                    df = self.open_csv_file(path)
+                except ParserError as e:
+                    print(f"{Fore.RED}Exception type: {type(e).__name__}{Style.RESET_ALL}")
+                    print(f"{Fore.RED}Exception: {e}{Style.RESET_ALL}")
+                    print('try to read as excel')
+                    df = self.open_excel_file(path)
+                    print(df.shape)
+                except UnicodeError as e:
+                    print(f"{Fore.RED}Exception type: {type(e).__name__}{Style.RESET_ALL}")
+                    print(f"{Fore.RED}Exception: {e}{Style.RESET_ALL}")
+                    print('try to read as excel')
+                    df = self.open_excel_file(path)
+                    print(df.shape)
+            except Exception as e:
+                print(f"{Fore.RED}Exception type: {type(e).__name__}{Style.RESET_ALL}")
+                print(f"{Fore.RED}Exception: {e}{Style.RESET_ALL}")
+                print('cant read as df')
+                df = None
+
+        print('opened df, return from open_df')
+        return df
+    
+    def drop_empty_columns(self, df):
+        if df is not None:
+            df = df.dropna(axis=1, how='all')
+            return df
+        else:
+            return df
     
     def return_csv(self, df, path):
         path_split = path.split('/')
         dataset = path_split[2]
-        table = path_split[3].split('.')[0]
+        if '.' in path_split[3]:
+            table_ext = path_split[3].split('.')
+            table = table_ext[0]
+            extension = table_ext[1]
+        else:
+            table = path_split[3]
+            extension = ""
         table = table.replace(" ", "_")
-        os.makedirs(f'data/datasets/prep_datasets/{path_split[2]}', exist_ok=True)
-        df.to_csv(f'data/datasets/prep_datasets/{dataset}/{table}.csv', index=False, sep=";")
+        table = table.replace("-", "_")
+        os.makedirs(f'data/prep_datasets/{path_split[2]}', exist_ok=True)
+        df.to_csv(f'data/prep_datasets/{dataset}/{table}_{extension}.csv', index=False, sep=";")
     
-        
-
+    def process_all_files(self):
+        for path in self.paths:
+            print("---------------------------------------------------")
+            print(Fore.GREEN + path + Style.RESET_ALL)
+            df = self.open_df(path)
+            print('this is df')
+            if df is not None:
+                # display(df.head(1))
+                df = self.transposed(df)
+                df = self.drop_empty_columns(df)
+                df = self.columns_formatter(df)
+                df = self.check_column_clean(df)
+                self.return_csv(df, path)
+                print(Fore.GREEN + f"{path} processed successfully!" + Style.RESET_ALL)
+                print("---------------------------------------------------")
+            else:
+                print(Fore.RED + f"{path} not processed!" + Style.RESET_ALL)
+                print("---------------------------------------------------")
+    
